e62a43ce406f41d3f2aac4d7d567cbf95b1e9d10
Steve Rivera <srivera.x1@gmail.com>
2018-01-21 01:15:13 -0500

    update docs

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 """
-This script assumes a moisture detector is connected to GPIO17 on a Raspberry Pi. Sensor is below:
+This script assumes a moisture detector is connected to SPI on the Raspberry Pi 3. Sensor is below:
 http://a.co/21NN4yP
 
 The moisture sensor is activated when removed from water or the moisture level reaches
 below the threshold. When this happens an e-mail is sent.
 
-**DEPENDENCIES:**
+_Table of measurements and average readings to come..._
+
+## Dependencies
 - Python `2.7.9`
 - Raspbian GNU/Linux 8 (jessie)
+  - with [hardware SPI enabled](https://www.raspberrypi-spy.co.uk/2014/08/enabling-the-spi-interface-on-the-raspberry-pi/)
 ```bash
 $ sudo pip install python-dotenv
 $ sudo pip install jinja2
 $ sudo pip install adafruit-mcp3008
 ```
 
-**Globally available as a script**
+### Globally available as a script
 ```bash
 $ sudo -i
 $ PROJECT_ROOT=/home/pi/Projects/moisture-sensor
@@ -30,6 +33,8 @@ $ exit
 
 **NOTE:** To use Amazon SES a TXT Record must be added to Namecheap's DNS Entries. Also, free-tier limits the FROM/TO to **verified** e-mails only. To send from `plantbot@my-domain.com`:
 
+**NOTE:** When adding the TXT record on Namecheap ignore amazon's `_amazonses.yourdomain.*` TXT name value and just put `@` in the host column
+
 1. Create a catch-all redirect e-mail on namecheap and point it to gmail
 2. Send verification e-mail to the above from the Amazon SES console
 3. ???
@@ -37,14 +42,17 @@ $ exit
 
 --------------------
 
+## Hardware Requirements
+
 To read specific moisture levels the sensor's analog signal must be used. Unfortunately, Raspberry PI doesn't support analog ootb so the following are needed:
 - Adafruit MCP3008 (ADC): https://www.adafruit.com/product/856
 - Half/Full breadboard: http://a.co/7MVedwJ
+- Link to image of circuit coming soon...
 
 Adafruit provides an easy to use library to interact with the MCP3008 and get readings from its channels. Along with examples:
 https://github.com/adafruit/Adafruit_Python_MCP3008
 
-**Wire up the MCP3008**
+### Wire up the MCP3008
 
 https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008#wiring
 
@@ -54,7 +62,7 @@ http://www.raspberrypi-spy.co.uk/2014/08/enabling-the-spi-interface-on-the-raspb
 
 --------------------
 
-**Resources**
+## Resources
 
 - https://www.modmypi.com/blog/raspberry-pi-plant-pot-moisture-sensor-via-analogue-signals
 - https://www.raspberrypi.org/forums/viewtopic.php?t=55754
@@ -63,9 +71,9 @@ http://www.raspberrypi-spy.co.uk/2014/08/enabling-the-spi-interface-on-the-raspb
 
 --------------------
 
-- [ ] **TODO:** better error handling for when device not found on configured GPIO#
+- [ ] **TODO:** better error handling for when device not found
 - [ ] **TODO:** calibrate it to soil: ~497-600 when soil is recently watered. slowly goes up to ~850 over 4hours
-- [x] ~~**TODO:** can we pull specific moisture levels? (See links below)~~
+- [x] ~~**TODO:** pull specific moisture levels?~~
 - [ ] **TODO:** upgrade to python 3?
 - [ ] **TODO:** keep count of warnings. if reached threshold send an e-mail
 - [ ] **TODO:** limit e-mails via configuration variables
